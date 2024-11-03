e1a0024476fc9a494ef95ca8454b6d57659efd91
JvFullidz123 <151520749+JvFullidz123@users.noreply.github.com>
2024-11-03 14:58:30 +0800

    Update Dispatcher_AddSlip.aspx.designer.cs
    Naa rakoy gi-add

--- Dispatcher_AddSlip.aspx.designer.cs
+++ Dispatcher_AddSlip.aspx.designer.cs
@@ -51,13 +51,13 @@ namespace Capstone
         protected global::System.Web.UI.WebControls.Label Label1;
 
         /// <summary>
-        /// Label3 control.
+        /// Employee_Role control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label3;
+        protected global::System.Web.UI.WebControls.Label Employee_Role;
 
         /// <summary>
         /// txtSearch control.
